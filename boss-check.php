aff2931549472aecfe174a511a0e141d0a08e258
allthat300 <40317007+allthat300@users.noreply.github.com>
2018-06-29 01:27:04 +0900

    Add files via upload

--- boss-check.php
+++ boss-check.php
@@ -100,26 +100,115 @@ $pdo = db_connect();
 
     <main role="main" class="col-md-11 ml-sm-auto col-lg-11 px-4">
 
-<h1 class="h1 my-3">部長確認</h1>
+      <h1 class="h1 my-3">部長確認</h1>
+      <h4>検索条件</h4>
+      <hr>
+      <form method="get" action="ZANGYO_CHECK.php">
+        <button class="btn btn-primary" type="submit" name='action' value='edit'>編集</button>
+        <button class="btn btn-primary" type="submit" name='action' value='delete'>削除</button>
+        <button class="btn btn-primary" type="submit" name='action' value='getURL'>URL生成</button>
+        <hr>
 
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered table-condensed table-responsive text-center" style="table-layout:fixed;">
+            <thead>
+              <tr>
+                <th style="width:50px;">実施日</th>
+                <th style="width:150px;">実施日</th>
+                <th style="width:100px;">種別</th>
+                <th style="width:150px;">名前</th>
+                <th style="width:150px;">部署</th>
+                <th style="width:150px;">グループ</th>
+                <th style="width:150px;">申請時間</th>
+                <th style="width:150px;">実施時間</th>
+                <th style="width:150px;">月間累計</th>
+                <th style="width:150px;">年間累計</th>
+                <th style="width:100px;">確認</th>
+                <th style="width:200px;">機種</th>
+                <th style="width:400px;">内容</th>
+                <th style="width:600px;">備考</th>
+              </tr>
+            </thead>
+            <tbody>
 
-    </main>
+              <tr>
+                <?php
+                try{
+                  $sql="SELECT zangyo.id,zangyo.zangyo_date,zangyo.app_time,zangyo.employee_id,employee.employee_name,case_id.category,zangyo.project,zangyo.project_detail,zangyo.remarks,zangyo.result_time,department.department_name,work_group.group_name
+                  from ((((zangyo LEFT OUTER JOIN employee ON zangyo.employee_id = employee.employee_id)
+                  LEFT OUTER JOIN case_id ON zangyo.case_id = case_id.case_id)
+                  LEFT OUTER JOIN department ON employee.department_id = department.department_id)
+                  LEFT OUTER JOIN work_group ON employee.group_id = work_group.group_id)
+                  ORDER BY zangyo_date DESC
+                  ";
+                  //                        where id=(select max(id) from zangyo)";
+                  $stmh=$pdo->prepare($sql);
+                  $stmh->execute();
+                  $count=$stmh->rowCount();
+                }catch(PDOException $Exception){
+                  print"エラー：".$Exception->getMessage();
+                }
+                if($count>0){
+                  while($row=$stmh->fetch(PDO::FETCH_ASSOC)){
+                    ?>
+                    <td><input type="checkbox" name="id[]" value="<?=htmlspecialchars($row['id'],ENT_QUOTES)?>"></td>
+                    <td><!--実施日--><?php
+
+                    $datetime = new DateTime($row['zangyo_date']);
+                    $week = array("日", "月", "火", "水", "木", "金", "土");
+                    $w = (int)$datetime->format('w');
+                    echo htmlspecialchars(substr($row['zangyo_date'],0,10),ENT_QUOTES) . " (" . $week[$w] . ")";
+                    ?></td> <!--<?php /* <? PHPの式 ?>は<? echo PHPの式 ?>の省略形 */ ?>-->
+                    <td><?=htmlspecialchars($row['category'],ENT_QUOTES)?></td><!--種別-->
+                    <td><?=htmlspecialchars($row['employee_name'],ENT_QUOTES)?></td><!--名前-->
+                    <td><?=htmlspecialchars($row['department_name'],ENT_QUOTES)?></td><!--部署-->
+                    <td><?=htmlspecialchars($row['group_name'],ENT_QUOTES)?></td><!--グループ-->
+                    <td><?=htmlspecialchars(substr($row['app_time'],0,-3),ENT_QUOTES)?></td><!--申請時間-->
+                    <td><!--実施時間-->
+                      <?php
+                      if(Is_null($row['result_time']) == TRUE){
+                        echo "";
+                      }else {
+                        echo htmlspecialchars(substr($row['result_time'],0,-3),ENT_QUOTES);
+                      }
+                      ?></td>
+                      <td><?php require("../php_libs/SUM_MONTH.php"); ?></td><!--月間累計-->
+                      <td><!--年間累計-->
+                        <?php
+                        require_once("../php_libs/FUNC_CHANGE_TO_APR1.php");
+                        require("../php_libs/SUM_YEAR.php");
+                        ?>
+                      </td>
+                      <td></td><!--確認-->
+                      <td><?=htmlspecialchars($row['project'],ENT_QUOTES)?></td><!--機種-->
+                      <td><?=htmlspecialchars($row['project_detail'],ENT_QUOTES)?></td><!--内容-->
+                      <td><?=htmlspecialchars($row['remarks'],ENT_QUOTES)?></td><!--備考-->
+                    </tr>
+                    <?php
+                  }
+                }
+                ?>
+              </tbody>
+            </table>
+          </div>
+
+        </main>
+      </div>
     </div>
-  </div>
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-<script src="/dist/js/vendor/popper.min.js"></script>
-<script src="/dist/js/bootstrap.min.js"></script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="/dist/js/vendor/popper.min.js"></script>
+    <script src="/dist/js/bootstrap.min.js"></script>
 
-<!-- Icons -->
-<script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-<script>
-feather.replace()
-</script>
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+    feather.replace()
+  </script>
 
 
 </body>
